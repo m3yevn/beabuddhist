435978042aebf8ac5439e354addba0fa5136a24d
Kevin Moe Myint Myat <mmm.myintmyat@gmail.com>
2020-01-29 11:00:36 +0800

    :fire: Remove API key from repo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyD2u0mS9GGrtRCI23dG2dDEBrfGgXziQmQ",
+    apiKey: "**Replace_With_Your_Own_API_Key**",
     authDomain: "beabuddhist-3ceda.firebaseapp.com",
     databaseURL: "https://beabuddhist-3ceda.firebaseio.com",
     projectId: "beabuddhist-3ceda",
